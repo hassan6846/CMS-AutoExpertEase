022afab73258275fd0a2ee1efc1e88e65012bdd8
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-07-01 13:19:02 +0500

    Update vite.config.js
    Deployed to netlify

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    proxy:{
+  server: {
+    proxy: {
       '/api': {
-        target: 'http://localhost:4001', // Adjust port if necessary
+        target: 'https://backend-autoexpertease-production-5fd2.up.railway.app', // Update to your deployed backend URL
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '') // Remove '/api' prefix
+        rewrite: (path) => path.replace(/^\/api/, '') // Remove '/api' prefix if needed
       }
     }
   }
